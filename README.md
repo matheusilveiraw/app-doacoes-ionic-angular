86af6902be67943d7d497004852e04aca4e746d2
matheusilveiraw <59832941+matheusilveiraw@users.noreply.github.com>
2024-03-17 19:27:59 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# app-doacoes-ionic-angular
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/matheusilveiraw/app-doacoes-ionic-angular)
---
+++ src/app/tab2/tab2.page.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab2',
+  templateUrl: 'tab2.page.html',
+  styleUrls: ['tab2.page.scss'],
+})
+export class Tab2Page {
+  constructor() {}
+
+  doarAlimentos() {
+    this.abrirUrl(
+      'https://www.google.com/maps/search/?api=1&query=doacao+alimentos'
+    );
+  }
+
+  doarRoupas() {
+    this.abrirUrl(
+      'https://www.google.com/maps/search/?api=1&query=doacao+roupas'
+    );
+  }
+
+  doarSangue() {
+    this.abrirUrl(
+      'https://www.google.com/maps/search/?api=1&query=doacao+sangue'
+    );
+  }
+
+  abrirUrl(url: string) {
+    window.open(url, '_blank').focus();
+  }
+}
